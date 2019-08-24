64766a2e8e4204fc9f2277ccb8b83b0dc6777af3
Luifer <luiferpalmera@gmail.com>
2019-08-24 16:55:53 -0500

    Defición de métodos en controlador

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -7,4 +7,28 @@ use Illuminate\Http\Request;
 class PersonaController extends Controller
 {
     //
+    public function obtenerPersonas(Request $request){
+        if($request->ajax()){
+
+        }
+        return view('index');
+    }
+    public function guardarPersona(Request $request){
+        if($request->ajax()){
+
+        }
+        return view('index');
+    }
+    public function actualizarPersona(Request $request){
+        if($request->ajax()){
+
+        }
+        return view('index');
+    }
+    public function eliminarPersona(Request $request){
+        if($request->ajax()){
+
+        }
+        return view('index');
+    }
 }
